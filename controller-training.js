597cae396acf358ff9cd427cc83bab659864657d
Erin Altenhof-Long <ealtenhof@google.com>
2014-06-16 11:05:14 -0700

    feat(controllerTraining): add patching for EventTarget and Node properties

--- controller-training.js
+++ controller-training.js
@@ -1,3 +1,7 @@
+patchProperties = function() {
+
+};
+
 collectProperties = function() {
     var objectProperties = {};
     var objectPropertyNames = Object.getOwnPropertyNames(Element.prototype);
@@ -7,7 +11,27 @@ collectProperties = function() {
     return objectProperties;
 };
 
+collectPropertiesNode = function() {
+    var objectProperties = {};
+    var objectPropertyNames = Object.getOwnPropertyNames(Node.prototype);
+    objectPropertyNames.forEach(function(prop) {
+      objectProperties[prop] = Node.prototype[prop];
+    });
+    return objectProperties;
+};
+
+collectPropertiesEventTarget = function() {
+    var objectProperties = {};
+    var objectPropertyNames = Object.getOwnPropertyNames(EventTarget.prototype);
+    objectPropertyNames.forEach(function(prop) {
+      objectProperties[prop] = EventTarget.prototype[prop];
+    });
+    return objectProperties;
+};
+
 var originalProperties = collectProperties();
+var originalPropertiesNode = collectPropertiesNode();
+var originalPropertiesEventTarget = collectPropertiesEventTarget();
 
 var controllerTrainer = {
   addManipulationListener: function(givenFunction) {
@@ -31,6 +55,16 @@ var controllerTrainer = {
         };
       }
     });
+    var eventTargetProperties = Object.getOwnPropertyNames(EventTarget.prototype);
+    eventTargetProperties.forEach(function(prop) {
+      var originalEventTarget = EventTarget.prototype[prop];
+      if(typeof originalEventTarget === 'function') {
+        EventTarget.prototype[prop] = function () {
+          givenFunction();
+          return originalEventTarget.apply(this, arguments);
+        };
+      }
+    });
   },
   handleManipulation: function() {
     return 'Manipulation Handled';
@@ -41,6 +75,22 @@ var controllerTrainer = {
       var alteredElement = Element.prototype[prop];
       if(typeof alteredElement === 'function') {
         Element.prototype[prop] = originalProperties[prop];
+      }
+    });
+
+    var objectPropertiesNode = Object.getOwnPropertyNames(Node.prototype);
+    objectPropertiesNode.forEach(function(prop) {
+      var alteredElementNode = Node.prototype[prop];
+      if(typeof alteredElementNode === 'function') {
+        Node.prototype[prop] = originalPropertiesNode[prop];
+      }
+    });
+
+    var objectPropertiesEventTarget = Object.getOwnPropertyNames(EventTarget.prototype);
+    objectPropertiesEventTarget.forEach(function(prop) {
+      var alteredElementEventTarget = EventTarget.prototype[prop];
+      if(typeof alteredElementEventTarget === 'function') {
+        EventTarget.prototype[prop] = originalPropertiesEventTarget[prop];
       }
     });
   }
--- controller-training_test.js
+++ controller-training_test.js
@@ -20,9 +20,21 @@ describe('controller training', function() {
       controllerTrainer.removeManipulationListener();
     });
 
+    it('should patch the functions of EventTarget.prototype', function() {
+      var objectProperties = Object.getOwnPropertyNames(EventTarget.prototype);
+      var testProperty = objectProperties[0];
+      var originalFunction = EventTarget.prototype[testProperty];
+      expect(EventTarget.prototype[testProperty]).toBe(originalFunction);
+      controllerTrainer.addManipulationListener();
+      expect(EventTarget.prototype[testProperty]).not.toBe(originalFunction);
+      controllerTrainer.removeManipulationListener();
+    });
+
     it('should patch the functions of Element.prototype to call the givenFunction param', function() {
       var testObj = {};
-      testObj.testFunction = function(){};
+      testObj.testFunction = function(){
+        return 'test function';
+      };
       spyOn(testObj, 'testFunction');
       controllerTrainer.addManipulationListener(testObj.testFunction);
       expect(testObj.testFunction).not.toHaveBeenCalled();
@@ -34,66 +46,66 @@ describe('controller training', function() {
 
 
     it('should detect getting element.innerHTML', function() {
-      var testObj = {};
-      testObj.testFunction = function(){};
-      spyOn(testObj, 'testFunction');
-      controllerTrainer.addManipulationListener(testObj.testFunction);
-      expect(testObj.testFunction).not.toHaveBeenCalled();
+      var testObj2 = {};
+      testObj2.testFunction = function(){};
+      spyOn(testObj2, 'testFunction');
+      controllerTrainer.addManipulationListener(testObj2.testFunction);
+      expect(testObj2.testFunction).not.toHaveBeenCalled();
       var element = document.createElement('div');
       var inner = element.innerHTML;
-      expect(testObj.testFunction).toHaveBeenCalled();
+      expect(testObj2.testFunction).toHaveBeenCalled();
       controllerTrainer.removeManipulationListener();
     });
 
 
     it('should detect setting element.innerHTML', function() {
-      var testObj = {};
-      testObj.testFunction = function(){};
-      spyOn(testObj, 'testFunction');
-      controllerTrainer.addManipulationListener(testObj.testFunction);
-      expect(testObj.testFunction).not.toHaveBeenCalled();
+      var testObj3 = {};
+      testObj3.testFunction = function(){};
+      spyOn(testObj3, 'testFunction');
+      controllerTrainer.addManipulationListener(testObj3.testFunction);
+      expect(testObj3.testFunction).not.toHaveBeenCalled();
       var element = document.createElement('div');
       element.innerHTML = 'blank';
-      expect(testObj.testFunction).toHaveBeenCalled();
+      expect(testObj3.testFunction).toHaveBeenCalled();
       controllerTrainer.removeManipulationListener();
     });
 
 
     it('should detect getting element.parentElement', function() {
-      var testObj = {};
-      testObj.testFunction = function(){};
-      spyOn(testObj, 'testFunction');
-      controllerTrainer.addManipulationListener(testObj.testFunction);
-      expect(testObj.testFunction).not.toHaveBeenCalled();
+      var testObj4 = {};
+      testObj4.testFunction = function(){};
+      spyOn(testObj4, 'testFunction');
+      controllerTrainer.addManipulationListener(testObj4.testFunction);
+      expect(testObj4.testFunction).not.toHaveBeenCalled();
       var element = document.createElement('div');
       var parent = element.parentElement;
-      expect(testObj.testFunction).toHaveBeenCalled();
+      expect(testObj4.testFunction).toHaveBeenCalled();
       controllerTrainer.removeManipulationListener();
     });
 
 
     it('should detect calling element.addEventListener', function() {
-      var testObj = {};
-      testObj.testFunction = function(){};
-      spyOn(testObj, 'testFunction');
-      controllerTrainer.addManipulationListener(testObj.testFunction);
-      expect(testObj.testFunction).not.toHaveBeenCalled();
+      var testObj5 = {};
+      testObj5.testFunction = function(){};
+      spyOn(testObj5, 'testFunction');
+      controllerTrainer.addManipulationListener(testObj5.testFunction);
+      expect(testObj5.testFunction).not.toHaveBeenCalled();
       var element = document.createElement('div');
       var parent = element.addEventListener();
-      expect(testObj.testFunction).toHaveBeenCalled();
+      expect(testObj5.testFunction).toHaveBeenCalled();
       controllerTrainer.removeManipulationListener();
     });
 
 
     it('should detect calling element.remove', function() {
-      var testObj = {};
-      testObj.testFunction = function(){};
-      spyOn(testObj, 'testFunction');
-      controllerTrainer.addManipulationListener(testObj.testFunction);
-      expect(testObj.testFunction).not.toHaveBeenCalled();
+      var testObj6 = {};
+      testObj6.testFunction = function(){};
+      spyOn(testObj6, 'testFunction');
+      controllerTrainer.addManipulationListener(testObj6.testFunction);
+      expect(testObj6.testFunction).not.toHaveBeenCalled();
       var element = document.createElement('div');
       element.remove();
-      expect(testObj.testFunction).toHaveBeenCalled();
+      expect(testObj6.testFunction).toHaveBeenCalled();
       controllerTrainer.removeManipulationListener();
     });
 
@@ -102,14 +114,14 @@ describe('controller training', function() {
       var parentElement = document.createElement('div');
       var referenceElement = document.createElement('div');
       parentElement.appendChild(referenceElement);
-      var testObj = {};
-      testObj.testFunction = function(){};
-      spyOn(testObj, 'testFunction');
-      controllerTrainer.addManipulationListener(testObj.testFunction);
-      expect(testObj.testFunction).not.toHaveBeenCalled();
+      var testObj7 = {};
+      testObj7.testFunction = function(){};
+      spyOn(testObj7, 'testFunction');
+      controllerTrainer.addManipulationListener(testObj7.testFunction);
+      expect(testObj7.testFunction).not.toHaveBeenCalled();
       var newElement = document.createElement('div');
       parentElement.insertBefore(newElement, referenceElement);
-      expect(testObj.testFunction).toHaveBeenCalled();
+      expect(testObj7.testFunction).toHaveBeenCalled();
       controllerTrainer.removeManipulationListener();
     });
 
@@ -117,13 +129,15 @@ describe('controller training', function() {
     it('should detect calling element.appendChild', function() {
       var parentElement = document.createElement('div');
       var childElement = document.createElement('div');
-      var testObj = {};
-      testObj.testFunction = function(){};
-      spyOn(testObj, 'testFunction');
-      controllerTrainer.addManipulationListener(testObj.testFunction);
-      expect(testObj.testFunction).not.toHaveBeenCalled();
+      var testObj8 = {};
+      testObj8.testFunction = function(){
+        return 'test function';
+      };
+      spyOn(testObj8, 'testFunction');
+      controllerTrainer.addManipulationListener(testObj8.testFunction);
+      expect(testObj8.testFunction).not.toHaveBeenCalled();
       parentElement.appendChild(childElement);
-      expect(testObj.testFunction).toHaveBeenCalled();
+      expect(testObj8.testFunction).toHaveBeenCalled();
       controllerTrainer.removeManipulationListener();
     });
   });
@@ -144,6 +158,11 @@ describe('controller training', function() {
       expect(Element.prototype[testProperty]).not.toBe(originalFunction);
       controllerTrainer.removeManipulationListener();
       expect(Element.prototype[testProperty]).toBe(originalFunction);
+    });
+  });
+  describe('patchProperties()', function() {
+    it('should return an array of DOM elements and their relevant properties', function() {
+
     });
   });
 });
