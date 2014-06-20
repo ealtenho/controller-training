ae8a4708714f96da6649de7a2ac7d52e713fcfec
Erin Altenhof-Long <ealtenhof@google.com>
2014-06-20 16:33:23 -0700

    test(): add test to try and diagnose async issue
    An issue has developed with an error being thrown
    after an asynchronous call completes. This test aimed
    to show if some prototype remained unpatched. Here, the
    listener is only called when expected, inside the timeout.
    All other manipulations do not call the listener, illustrating
    that the prototypes are unpatched after that point.
    
    #22

--- async_test.js
+++ async_test.js
@@ -6,6 +6,11 @@ describe('Asynchronous controller execution detection', function(){
     $controller = _$controller_;
     $rootScope = _$rootScope_;
   }));
+  beforeEach(function() {
+    patchServices.listener = function() {
+      console.log('For tests, just print this error.');
+    }
+  });
   it('should handle asynchronous DOM manipulations', inject(function($rootScope) {
         var timeoutCompleted = false;
 
@@ -23,7 +28,7 @@ describe('Asynchronous controller execution detection', function(){
           var ctrl = $controller(controllerMock);
           }
           catch(e){
-
+            console.log('Caught an error making controller: ' + e);
           }
           $rootScope.$apply();
         });
@@ -36,4 +41,50 @@ describe('Asynchronous controller execution detection', function(){
           expect(patchServices.listener).toHaveBeenCalled();
         });
       }));
+  it('should unpatch elements after asynchronous execution', inject(function($rootScope) {
+    var timeoutCompleted = false;
+    var objectProperties = Object.getOwnPropertyNames(Element.prototype);
+    var objectPropertiesNode = Object.getOwnPropertyNames(Node.prototype);
+    var objectPropertiesEventTarget = Object.getOwnPropertyNames(EventTarget.prototype);
+    var objectPropertiesDocument = Object.getOwnPropertyNames(Document.prototype);
+    var testProperty = objectProperties[0];
+    var originalFunction = Element.prototype[testProperty];
+    var originalFunctionNode = Node.prototype[testProperty];
+    var originalFunctionEventTarget = EventTarget.prototype[testProperty];
+    var originalFunctionDocument = Document.prototype[testProperty];
+    expect(Element.prototype[testProperty]).toBe(originalFunction);
+    var elem = document.createElement('div');
+        runs(function() {
+          spyOn(patchServices, 'listener');
+          var controllerMock = function($timeout) {
+              $timeout(function() {
+                var element = document.createElement('a');
+                element.getAttribute('id');
+                timeoutCompleted = true;
+              }, 0);
+            };
+          try{
+          elem.getAttribute('id');
+          var ctrl = $controller(controllerMock);
+          }
+          catch(e){
+            console.log('Caught an error making controller: ' + e);
+          }
+          elem.getAttribute('id');
+        });
+
+        waitsFor(function() {
+          elem.getAttribute('id');
+          return timeoutCompleted;
+        }, 'controller execution', 100);
+
+        runs(function() {
+          elem.getAttribute('id');
+          expect(Element.prototype[testProperty]).toBe(originalFunction);
+          expect(Node.prototype[testProperty]).toBe(originalFunctionNode);
+          expect(EventTarget.prototype[testProperty]).toBe(originalFunctionEventTarget);
+          expect(Document.prototype[testProperty]).toBe(originalFunctionDocument);
+          expect(patchServices.listener.callCount).toBe(2);
+        });
+  }));
 });
