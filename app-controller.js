a64ab24729dd66f93859e9557281fe1c98d4d476
Erin Altenhof-Long <ealtenhof@google.com>
2014-06-20 15:08:02 -0700

    feat(sampleApp): add examples to sample controller-training application

--- app-controller.js
+++ app-controller.js
@@ -1,19 +1,47 @@
 angular.module('sampleApp').
-  controller('SampleAppController', function() {
+  controller('SampleAppController', ['$timeout', '$scope', function($timeout, $scope) {
 
-    this.clicked = false;
-    // var elm = document.body.children[1];
-    // elm.innerHTML = 'test';
-
+    /**
+    * 1. DOM manipulation method of changing button
+    */
     // var btn = document.getElementById('trainingWheels');
     // btn.onclick = function() {
     //   btn.innerHTML = 'Training wheels off!';
-    //   var elm = document.createElement('a');
-    //   document.add(elm);
     // }
 
-    // var newDiv = document.createElement('div');
-    // newDiv.innerHTML = 'Training wheels off!';
+    /**
+    * 1. Angular method of changing button
+    */
+    this.clicked = false;
 
-    //document.body.children[1].remove();
-  });
+    /**
+    * 2. DOM manipulation method of adding dynamic elements
+    */
+    this.bicycles = ['Google bike', 'Mountain Bike', 'Conference Bike'];
+
+    // var bikeDiv = document.getElementById('bicycles');
+    // for(var i = 0; i < this.bicycles.length; i++)
+    // {
+    //     var newDiv = document.createElement('div');
+    //     newDiv.innerHTML = this.bicycles[i];
+    //     bikeDiv.appendChild(newDiv);
+    // }
+
+
+    /**
+    * 3. DOM manipulation method (using JQuery) of removing elements
+    * Also demonstrates catching asynchronous actions
+    */
+    // $timeout(function() {
+    //     $('#remove').remove();
+    // }, 3000);
+
+    $scope.removeTime = false;
+    /**
+    * 3. Angular method for removal
+    */
+      $timeout(function() {
+        console.log('Running');
+        $scope.removeTime = true;
+      }.bind($scope), 3000);
+  }]);
