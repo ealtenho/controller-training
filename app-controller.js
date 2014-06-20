5f3627c99c2d593797517f9e885e393e53a9eb0c
Erin Altenhof-Long <ealtenhof@google.com>
2014-06-20 11:01:58 -0700

    feat(): add sample app functionality to showcase error throwing

--- app-controller.js
+++ app-controller.js
@@ -1,14 +1,19 @@
 angular.module('sampleApp').
   controller('SampleAppController', function() {
-    var btn = document.getElementById('trainingWheels');
-    btn.onclick = function() {
-      btn.innerHTML = 'Training wheels off!';
-      //var elm = document.createElement('a');
-      //document.add(elm);
-    }
 
-    var newDiv = document.createElement('div');
-    newDiv.innerHTML = 'Training wheels off!';
+    this.clicked = false;
+    // var elm = document.body.children[1];
+    // elm.innerHTML = 'test';
 
-    btn.remove();
+    // var btn = document.getElementById('trainingWheels');
+    // btn.onclick = function() {
+    //   btn.innerHTML = 'Training wheels off!';
+    //   var elm = document.createElement('a');
+    //   document.add(elm);
+    // }
+
+    // var newDiv = document.createElement('div');
+    // newDiv.innerHTML = 'Training wheels off!';
+
+    //document.body.children[1].remove();
   });
